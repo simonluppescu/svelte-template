ce33aa57674b5d5559b295b6bc56fe4ca2dfae1b
Simon <simon.luppescu@gmail.com>
2024-08-27 23:34:21 +0900

    Formatted files and replaced with super basic app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,13 @@
 <script>
-	import Header from './Header.svelte';
-	import '../app.css';
+  import "../app.css";
 </script>
 
 <div class="app">
-	<Header />
+  <h1>This is part of the layout.</h1>
+  <a href="/">Go home</a>
+  <a href="/about">Go to about</a>
 
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+  <main>
+    <slot />
+  </main>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
